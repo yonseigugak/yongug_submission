e1aa4e7877dac84336375ca1b104a23bd7d94eca
jsw0630 <jsw0630@yonsei.ac.kr>
2025-07-01 15:58:22 +0900

    name

--- lib/getSheetNames.ts
+++ lib/getSheetNames.ts
@@ -10,8 +10,8 @@ export async function getSheetNames(): Promise<string[]> {
 
   const auth = new google.auth.GoogleAuth({
     credentials: {
-      client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-      private_key:  process.env.GOOGLE_SHEETS_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key:  process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     },
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
   });
